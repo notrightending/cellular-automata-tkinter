007a0f6f1e8ceb2bf5ede5f739ee081500459f65
NightPuppet <waitingfortears@protonmail.com>
2023-05-05 00:36:47 +0300

    Refactored code

---
+++ board.py
@@ -0,0 +1,41 @@
+import random
+
+def create_board(cols, rows):
+    """Initialize board with dead cells."""
+    board = [[0 for _ in range(cols)] for _ in range(rows)]
+    return board
+
+def create_random_board(cols, rows):
+    """Create a board with random living and dead cells."""
+    board = [[random.randint(0, 1) for _ in range(cols)] for _ in range(rows)]
+    return board
+
+def count_neighbours(row0, column0, board, states):
+    """Count the number of living neighbours for a given cell."""
+    neighbours = [0] * states
+    for d_row in range(-1, 2):
+        for d_col in range(-1, 2):
+            if d_row != 0 or d_col != 0:
+                row = row0 + d_row
+                column = column0 + d_col
+                if 0 <= row < len(board) and 0 <= column < len(board[row]):
+                    neighbours[board[row][column]] += 1
+    return neighbours
+
+def compute_next_board(states, current_board, next_board):
+    """Compute the next state of the board based on the current state."""
+    DEAD = 0
+    ALIVE = 1
+    for row in range(len(current_board)):
+        for col in range(len(current_board[row])):
+            neighbours = count_neighbours(row, col, current_board, states)
+            if current_board[row][col] == ALIVE:
+                if neighbours[ALIVE] == 2 or neighbours[ALIVE] == 3:
+                    next_board[row][col] = ALIVE
+                else:
+                    next_board[row][col] = DEAD
+            elif current_board[row][col] == DEAD:
+                if neighbours[ALIVE] == 3:
+                    next_board[row][col] = ALIVE
+                else:
+                    next_board[row][col] = DEAD
---
+++ gui.py
@@ -0,0 +1,85 @@
+import tkinter as tk
+from board import create_board, create_random_board, compute_next_board
+
+class GameOfLifeGUI:
+    def __init__(self, root, cols, rows, canvas_width, canvas_height):
+        self.root = root
+        self.cols = cols
+        self.rows = rows
+        self.canvas_width = canvas_width
+        self.canvas_height = canvas_height
+        self.cell_height = canvas_height // rows
+        self.cell_width = canvas_width // cols
+        self.current_board = create_board(cols, rows)
+        self.next_board = create_board(cols, rows)
+        self.simulation_running = False
+        self.rectangle_id_to_cell = {}
+        self.initialize_gui()
+
+    def initialize_gui(self):
+        self.canvas = tk.Canvas(self.root, width=self.canvas_width, height=self.canvas_height, bg='white')
+        self.canvas.pack(anchor=tk.CENTER, expand=True)
+        self.create_buttons()
+        self.render(self.current_board)
+
+    def create_buttons(self):
+        button_frame = tk.Frame(self.root)
+        button_step = tk.Button(button_frame, text='Next step', command=self.button_func)
+        button_rand = tk.Button(button_frame, text='Randomize', command=self.button_random)
+        button_sim =  tk.Button(button_frame, text='Simulate', command=self.button_simulate)
+        button_clear = tk.Button(button_frame, text='Clear', command=self.button_clearf)
+        button_stop = tk.Button(button_frame, text='Stop', command=self.button_stopf)
+        button_step.pack(side=tk.LEFT, anchor=tk.CENTER, padx=5)
+        button_rand.pack(side=tk.LEFT, anchor=tk.CENTER, padx=5)
+        button_sim.pack(side=tk.LEFT, anchor=tk.CENTER, padx=5)
+        button_clear.pack(side=tk.LEFT, anchor=tk.CENTER, padx=5)
+        button_stop.pack(side=tk.LEFT, anchor=tk.CENTER, padx=5)
+        button_frame.pack()
+        button_frame.place(anchor=tk.CENTER, relx=0.5, rely=0.98)
+
+    def button_func(self):
+        compute_next_board(2, self.current_board, self.next_board)
+        self.current_board, self.next_board = self.next_board, self.current_board
+        self.render(self.current_board)
+
+    def button_simulate(self):
+        self.simulation_running = True
+        self.run_simulation()
+
+    def run_simulation(self):
+        if self.simulation_running:
+            self.button_func()
+            self.root.after(100, self.run_simulation)
+
+    def button_stopf(self):
+        self.simulation_running = False
+
+    def button_random(self):
+        self.current_board = create_random_board(self.cols, self.rows)
+        self.render(self.current_board)
+
+    def button_clearf(self):
+        self.current_board = create_board(self.cols, self.rows)
+        self.render(self.current_board)
+
+    def render(self, board):
+        self.canvas.delete("all")
+        for row in range(self.rows):
+            for col in range(self.cols):
+                x = col * self.cell_width
+                y = row * self.cell_height
+                if (self.current_board[row][col] == 1):
+                    fill_color = 'black'
+                else:
+                    fill_color = 'white'
+                rectangle_id = self.canvas.create_rectangle(x, y, x+self.cell_width, y+self.cell_height, width=1, outline='gray', fill=fill_color)
+                self.rectangle_id_to_cell[rectangle_id] = (row, col)
+                self.canvas.tag_bind(rectangle_id, '<Button-1>', lambda e, rect_id=rectangle_id: self.on_rectangle_click(rect_id))
+
+    def on_rectangle_click(self, rect_id):
+        row, col = self.rectangle_id_to_cell[rect_id]
+        if self.current_board[row][col] == 1:
+            self.current_board[row][col] = 0
+        else:
+            self.current_board[row][col] = 1
+        self.render(self.current_board)
